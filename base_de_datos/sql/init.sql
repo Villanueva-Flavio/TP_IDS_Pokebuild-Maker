6c09750f8c22a6c209b2b674c1736992dc5fa12d
SrMaximiliano <maxisorin@gmail.com>
2024-06-01 14:01:27 -0300

    [FIX][#59] Creacion tabla Pokemon.

--- base_de_datos/sql/init.sql
+++ base_de_datos/sql/init.sql
@@ -1,10 +1,10 @@
-CREATE TABLE users (
+CREATE TABLE POKEMON (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    podekex_id INT,
-    level INT(3),
+    podekex_id INT NOT NULL,
+    level INT(3) NOT NULL,
     name VARCHAR,
-    ability_1 VARCHAR NULL,
-    ability_2 VARCHAR NULL,
-    ability_3 VARCHAR NULL,
-    ability_4 VARCHAR NULL,
+    ability_1 VARCHAR NOT NULL,
+    ability_2 VARCHAR,
+    ability_3 VARCHAR,
+    ability_4 VARCHAR,
 );
