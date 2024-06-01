a83006772601c818f7e1b9391213fbe6c73d1636
SrMaximiliano <maxisorin@gmail.com>
2024-06-01 13:49:20 -0300

    Este commit es de la tarjeta: Base de datos, de la tarea de id: [#59] "Tabla Pokemon" Cree la Tabla de Pokémon como dice la tarea Sorin Maximiliano

---
+++ base_de_datos/sql/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    podekex_id INT,
+    level INT(3),
+    name VARCHAR,
+    ability_1 VARCHAR NULL,
+    ability_2 VARCHAR NULL,
+    ability_3 VARCHAR NULL,
+    ability_4 VARCHAR NULL,
+);
