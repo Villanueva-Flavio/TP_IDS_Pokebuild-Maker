64f45a42ca73f8fc44e9952594b0f136f98a8f63
SrMaximiliano <msorin@fi.uba.ar>
2024-06-01 14:22:44 -0300

    [FIX][#59] Typo en tabla de Pokemon

--- base_de_datos/sql/init.sql
+++ base_de_datos/sql/init.sql
@@ -1,10 +1,10 @@
 CREATE TABLE POKEMON (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    podekex_id INT NOT NULL,
+    podekex_id INT(4) NOT NULL,
     level INT(3) NOT NULL,
-    name VARCHAR,
-    ability_1 VARCHAR NOT NULL,
-    ability_2 VARCHAR,
-    ability_3 VARCHAR,
-    ability_4 VARCHAR,
+    name VARCHAR(12),
+    ability_1 VARCHAR(30) NOT NULL,
+    ability_2 VARCHAR(30),
+    ability_3 VARCHAR(30),
+    ability_4 VARCHAR(30)
 );
