c74c284e3001ae149d8aed721ccf9817236cfeb5
Miranda Portocarrero <portocarreromiranda@gmail.com>
2024-06-05 00:26:44 -0300

    [ADD] Implement functionality to display pokemon data in JSON format

--- app/api.py
+++ app/api.py
@@ -1,7 +1,22 @@
-from flask import Blueprint
+from flask import jsonify, Blueprint
+from sqlalchemy import create_engine, text
 
 api_blueprint = Blueprint('api', __name__)
 
+# uso el puerto 3306 para MySQL ya que es el puerto interno de MySQL en el contenedor
+engine = create_engine('mysql+mysqlconnector://root:1@pokebuild-db:3306/pokebuildmaker')
+
+@api_blueprint.route('/api/pokemons', methods=['GET'])
+def get_pokemons():
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT * FROM POKEMON"))
+        pokemons = []
+        for row in result:
+            pokemon_dict = dict(row)
+            pokemons.append(pokemon_dict)
+    return jsonify(pokemons)
+
+# Otros endpoints de tu API
 @api_blueprint.route('/api', methods=['GET'])
 def api_route():
     return 'Hello world from API!'
