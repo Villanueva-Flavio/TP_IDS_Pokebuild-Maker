ae66a1e0ea4e18adebd1a806c47f3c7b1e5d3524
FacundoXu <fxu@fi.uba.ar>
2024-06-06 17:01:32 -0300

    [FIX][#5][#6] Fixes

--- app/api.py
+++ app/api.py
@@ -7,7 +7,7 @@ api_blueprint = Blueprint('api', __name__)
 # uso el puerto 3306 para MySQL ya que es el puerto interno de MySQL en el contenedor
 engine = create_engine('mysql+mysqlconnector://root:1@pokebuild-db:3306/pokebuildmaker')
 
-@api_blueprint.route('/api/users', methods=['GET'])
+@api_blueprint.route('/api/users_profiles', methods=['GET'])
 def get_users_profiles():
     with engine.connect() as connection:
         result = connection.execute(text("SELECT username, email, profile_picture FROM USER"))
@@ -17,7 +17,7 @@ def get_users_profiles():
             users.append(user_dict)
     return jsonify(users)
 
-@api_blueprint.route('/api/user/<int:user_id>', methods=['GET'])
+@api_blueprint.route('/api/user_profile/<int:user_id>', methods=['GET'])
 def get_user_profile_by_id(user_id):
     try:
         with engine.connect() as connection:
