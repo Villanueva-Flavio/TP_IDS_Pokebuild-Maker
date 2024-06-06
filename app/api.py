7bc50e9598f1cb7dd366dfb9174b1b29bbaa8f57
SrMaximiliano <msorin@fi.uba.ar>
2024-06-05 23:02:30 -0300

    [FIX][#21] Cambie el codigo para que sea mas similar a otros GET y arregle un problema donde no me aceptaba el {id}

--- app/api.py
+++ app/api.py
@@ -9,23 +9,12 @@ engine = create_engine('mysql+mysqlconnector://root:1@pokebuild-db:3306/pokebuil
 @api_blueprint.route('/build/<id>', methods = ['GET'])
 def get_user(id):
     with engine.connect() as connection:
-        result = connection.execute(text("SELECT * FROM BUILDS WHERE id = {id}"))
-    if result.rowcount !=0:
-        data = {}
-        row = result.first()
-        data['id'] = row['id']
-        data['build_name'] = row['build_name']
-        data['pokemon_id_1'] = row['pokemon_id_1']
-        data['pokemon_id_2'] = row['pokemon_id_2']
-        data['pokemon_id_3'] = row['pokemon_id_3']
-        data['pokemon_id_4'] = row['pokemon_id_4']
-        data['pokemon_id_5'] = row['pokemon_id_5']
-        data['pokemon_id_6'] = row['pokemon_id_6']
-        data['timestamp'] = row['timestamp']
-        data['owner_id'] = row['owner_id']
-        return jsonify(data), 200
-    else:
-        return jsonify({"message": "El usuario no existe"}), 404
+        result = connection.execute(text(f"SELECT * FROM BUILDS WHERE id = {id}"))
+        build = []
+        for row in result:
+            build_dict = dict(row)
+            build.append(build_dict)
+    return jsonify(build)
 
 
 @api_blueprint.route('/api/pokemons', methods=['GET'])
