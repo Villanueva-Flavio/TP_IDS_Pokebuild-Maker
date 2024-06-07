3b68294b8c419bf72954df55cfcd54700295527c
FacundoXu <fxu@fi.uba.ar>
2024-06-06 21:57:21 -0300

    [FIX] USERS querys changes

--- app/api.py
+++ app/api.py
@@ -23,9 +23,9 @@ BUILD_ID_ROUTE = '/api/build/<build_id>'
 BUILD_ID_QUERY = "SELECT * FROM BUILDS WHERE ID = "
 
 USERS_ROUTE = '/api/users_profiles'
-USERS_QUERY = "SELECT username, email, profile_picture FROM USER"
+USERS_QUERY = "SELECT u.id, u.username, u.profile_picture, (SELECT COUNT(*) FROM POKEMON p WHERE p.owner_id = u.id) AS pokemon_count, (SELECT COUNT(*) FROM BUILDS b WHERE b.owner_id = u.id) AS build_count FROM USER u;"
 USER_ID_ROUTE = '/api/user_profile/<user_id>'
-USER_ID_QUERY = "SELECT username, email, profile_picture FROM USER WHERE ID = "
+USER_ID_QUERY = "SELECT u.id, u.username, u.profile_picture, (SELECT COUNT(*) FROM POKEMON p WHERE p.owner_id = u.id) AS pokemon_count, (SELECT COUNT(*) FROM BUILDS b WHERE b.owner_id = u.id) AS build_count FROM USER u WHERE id = "
 
 api_blueprint = Blueprint('api', __name__)
 db_url = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
