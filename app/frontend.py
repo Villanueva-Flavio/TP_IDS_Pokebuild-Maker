b0cd377e517ebaf774050401b839f13101e310b7
SrMaximiliano <msorin@fi.uba.ar>
2024-06-10 16:06:13 -0300

    [ADD][#34] Cree diseño del formulario para agregar Pokemons

--- app/frontend.py
+++ app/frontend.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 frontend_blueprint = Blueprint('frontend', __name__)
 
@@ -10,3 +10,16 @@ def index():
 @frontend_blueprint.route('/pop-up-test')
 def pop_up_test():
     return render_template('pop-up-test.html')
+
+@frontend_blueprint.route('/formulario_añadir_pokemon', methods=["POST", "GET"])
+def formulario_añadir_pokemon():
+    if request.method == "POST":
+        name = request.form.get("pname")
+        LVL = request.form.get("pLVL")
+        Pokemon = request.form.get("ppokemon")
+        abilidad_1 = request.form.get("pabilidad_1")
+        abilidad_2 = request.form.get("pabilidad_2")
+        abilidad_3 = request.form.get("pabilidad_3")
+        abilidad_4 = request.form.get("pabilidad_4")
+        return render_template("home.html", name=name,LVL=LVL,Pokemon=Pokemon,abilidad_1=abilidad_1,abilidad_2=abilidad_2,abilidad_3=abilidad_3,abilidad_4=abilidad_4)
+    return render_template("formulario_añadir_pokemon.html")
