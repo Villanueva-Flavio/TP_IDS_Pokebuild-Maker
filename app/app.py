6512b2469a294448bc67f69a7708368c17901d85
Flavio Villanueva <fvillanueva@fi.uba.ar>
2024-06-10 01:42:59 -0300

    [ADD] Assemble of home

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from api import api_blueprint  # Importa desde api.py
+from frontend import frontend_blueprint  # Importa desde frontend.py
 
 app = Flask(__name__)
 app.register_blueprint(api_blueprint)
+app.register_blueprint(frontend_blueprint)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
