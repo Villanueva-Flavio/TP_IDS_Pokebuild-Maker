89733f5fedc4408b6cfda4e84d7863521ce53417
FacundoXu <fxu@fi.uba.ar>
2024-06-15 00:21:57 -0300

    [ADD][#37] dynamic coverage images

--- app/static/js/busqueda_abilidades.js
+++ app/static/js/busqueda_abilidades.js
@@ -1,22 +1,48 @@
 const pokemonInput = document.getElementById('pokemon_imagen_cuadrado');
 const tipoImagenes = document.querySelectorAll('.posibles_tipo');
+const goodAgainstContainer = document.querySelector('.good_against div');
+const badAgainstContainer = document.querySelector('.bad_against div');
 
 function actualizarImagenesTipos(tipos) {
     tipoImagenes.forEach((imagen, index) => {
-
         if (tipos[index]) {
             imagen.src = `static/images/tipos_pokemon/${tipos[index]}.png`;
             imagen.style.display = 'inline-block';
-            
         } else {
             imagen.style.display = 'none';
         }
     });
 }
 
+function actualizarGoodAgainst(goodAgainst) {
+    // Limpiar contenedor
+    goodAgainstContainer.innerHTML = '';
+
+    // Agregar nuevas imágenes
+    goodAgainst.forEach(tipo => {
+        const img = document.createElement('img');
+        img.src = `static/images/tipos_pokemon/${tipo}.png`;
+        img.classList.add('tipo_imagen', 'tipo_pokemon_Good_Against');
+        goodAgainstContainer.appendChild(img);
+    });
+}
+
+function actualizarBadAgainst(badAgainst) {
+    // Limpiar contenedor
+    badAgainstContainer.innerHTML = '';
+
+    // Agregar nuevas imágenes
+    badAgainst.forEach(tipo => {
+        const img = document.createElement('img');
+        img.src = `static/images/tipos_pokemon/${tipo}.png`;
+        img.classList.add('tipo_imagen', 'tipo_pokemon_Bad_Against');
+        badAgainstContainer.appendChild(img);
+    });
+}
+
 pokemonInput.addEventListener('change', async () => {
     const pokemonNombre = pokemonInput.value.toLowerCase();
-    const { movimientos, tipos } = await obtenerDatosPokemon(pokemonNombre);
+    const { movimientos, tipos, good_against, bad_against } = await obtenerDatosPokemon(pokemonNombre);
     const movimientosList = document.getElementById('habilidades');
     movimientosList.innerHTML = '';
 
@@ -27,6 +53,8 @@ pokemonInput.addEventListener('change', async () => {
     });
 
     actualizarImagenesTipos(tipos);
+    actualizarGoodAgainst(good_against);
+    actualizarBadAgainst(bad_against);
 });
 
 async function obtenerDatosPokemon(pokemonNombre) {
@@ -39,15 +67,42 @@ async function obtenerDatosPokemon(pokemonNombre) {
             const datosPokemon = await respuesta.json();
             const movimientos = datosPokemon.moves.map(movimiento => movimiento.move.name);
             const tipos = datosPokemon.types.map(type => type.type.name);
-            return { movimientos, tipos };
+            const url_tipos = datosPokemon.types.map(type => type.type.url);
+
+            let good_against = new Set();
+            let bad_against = new Set();
+
+            for (let url of url_tipos) {
+                const datos = await fetch(url);
+
+                if (datos.ok) {
+                    const datosTipos = await datos.json();
+                    const buenos_contra = datosTipos.damage_relations.double_damage_to.map(tipo => tipo.name);
+
+                    for (let tipo of buenos_contra) {
+                        good_against.add(tipo);
+                    }
+
+                    const malos_contra = datosTipos.damage_relations.double_damage_from.map(tipo => tipo.name);
+
+                    for (let tipo of malos_contra) {
+                        bad_against.add(tipo);
+                    }
+                }
+            }
+
+            good_against = [...good_against];
+            bad_against = [...bad_against];
+
+            return { movimientos, tipos, good_against, bad_against };
 
         } else {
             console.error(`Error: No se pudo obtener la información del Pokémon ${pokemonNombre}. Código de estado: ${respuesta.status}`);
-            return { movimientos: [], tipos: [] };
+            return { movimientos: [], tipos: [], good_against: [], bad_against: [] };
         }
 
     } catch (error) {
         console.error("Error al obtener los datos del Pokémon:", error);
-        return { movimientos: [], tipos: [] };
+        return { movimientos: [], tipos: [], good_against: [], bad_against: [] };
     }
 }
