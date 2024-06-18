932e5ff6732fd35115bb94eaba8008de3024a1cc
Melanie Alaniz <166476867+Mel056@users.noreply.github.com>
2024-06-18 02:19:06 -0300

    [ADD][#55] build searchbar to remove

---
+++ app/static/js/delete_build.js
@@ -0,0 +1,30 @@
+function delete_build_by_id(){
+    const build_selected = document.getElementById('editable-select');
+    // El id de la build seleccionada
+    const build_id = build_selected.value;
+    // BUILD_DELETE_ROUTE
+    fetch(`/api/build_delete/${build_id_id}`, {
+        method: 'POST'
+    })
+    .then(response => {
+        if (!response.ok) {
+            console.log('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(result => {
+        // Eliminar el build de la lista de opciones
+        const optionToRemove = document.querySelector(`option[value="${build_id_id}"]`);
+        if (optionToRemove) {
+            optionToRemove.remove();
+        }
+        
+        console.log('Build deleted successfully');
+    })
+    .catch(error => {
+        console.error('Failed to delete build:', error);
+    });
+}
+
+document.getElementById('delete_build_btn')
+.addEventListener('click', delete_build_by_id);
