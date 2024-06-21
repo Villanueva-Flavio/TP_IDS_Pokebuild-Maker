7a02a2986e8919362799b233a0ec43cca2383c11
FacundoXu <fxu@fi.uba.ar>
2024-06-21 17:25:29 -0300

    [ADD] adding dynamic images script

--- app/static/js/busqueda_abilidades.js
+++ app/static/js/busqueda_abilidades.js
@@ -55,3 +55,104 @@ document.getElementById('poke_name').addEventListener('change', function() {
         document.getElementById('poke_habilidad_4').value = "";
     }
 });
+
+$(document).ready(function() {
+    $('.select-pokemon').select2({
+        tags: true,
+        placeholder: "Busca a tu pokemon...",
+        allowClear: true
+    });
+
+    $('.select-ability').select2({
+        tags: true,
+        placeholder: "Elige tu movimiento...",
+        allowClear: true
+    });
+var owner_id = "{{ owner_id }}";
+var pokemon_id;
+$('.select-pokemon').on('change', function() {
+    pokemon_id = $(this).val();
+
+    console.log(pokemon_id);
+    fetch('/api/moves/' + pokemon_id)
+        .then(response => response.json())
+        .then(data => {
+            var move_list = data['moves'];
+            console.log(move_list);
+            $('#ability_1, #ability_2, #ability_3, #ability_4').each(function() {
+                $(this).empty();
+                $(this).append('<option value=""></option>');
+                for(var i = 0; i < move_list.length; i++) {
+                    $(this).append('<option value="' + move_list[i] + '">' + move_list[i] + '</option>');
+                }
+            });
+        });
+
+    fetch('/api/types/' + pokemon_id)
+        .then(response => response.json())
+        .then(data => {
+            var pokemon_type = data['pokemon_type']
+            var good_against = data['good_against']
+            var bad_against = data['bad_against']
+            console.log('Pokemon Type: ' + pokemon_type)
+            console.log('Good Against: ' + good_against)
+            console.log('Bad Against: ' + bad_against)
+
+            $('.type-container img').remove();
+            $('#good img').remove();
+            $('#bad img').remove();
+
+            pokemon_type.forEach(type => {
+                $('.type-container').append('<img src="/static/images/tipos_pokemon/' + type + '.png" alt="type" class="img-type">')
+            })
+
+            good_against.forEach(type => {
+                $('#good').append('<img src="/static/images/tipos_pokemon/' + type + '.png" alt="type" class="img-type-against">')
+            })
+
+            bad_against.forEach(type => {
+                $('#bad').append('<img src="/static/images/tipos_pokemon/' + type + '.png" alt="type" class="img-type-against">')
+            })
+
+            while (pokemon_id.length < 3) { pokemon_id = '0' + pokemon_id; }
+
+            var pokemon_image_url = 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/' + pokemon_id + '.png';
+
+            $('#pokemon-image').attr('src', pokemon_image_url)
+        })
+});
+$('#add_pokemon_btn').on('click', function() {
+    var pokemon_name = $('#poke_name').val();
+    var pokemon_level = $('#poke_LVL').val();
+    var pokemon_species = $('#pokemon_especie').val();
+    var pokemon_ability_1 = $('#ability_1').val();
+    var pokemon_ability_2 = $('#ability_2').val();
+    var pokemon_ability_3 = $('#ability_3').val();
+    var pokemon_ability_4 = $('#ability_4').val();
+
+    fetch('/api/add_pokemon', {
+        method: 'POST',
+        body: JSON.stringify({
+            'name': pokemon_name,
+            'level': pokemon_level,
+            'ability_1': pokemon_ability_1,
+            'ability_2': pokemon_ability_2,
+            'ability_3': pokemon_ability_3,
+            'ability_4': pokemon_ability_4,
+            'pokedex_id': pokemon_id,
+            'owner_id': owner_id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Respuesta del servidor:');
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error al enviar la solicitud:', error);
+    });
+});
+});
