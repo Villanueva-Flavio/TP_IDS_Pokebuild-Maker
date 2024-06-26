405648c9041314bab6329c87cab2f6734024921b
kraskarocio <rkraska@fi.uba.ar>
2024-06-26 18:38:33 -0300

    [ADD] alerts login+register

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -2,6 +2,11 @@ document.getElementById('login_form').addEventListener('submit', function(event)
     event.preventDefault();
     const email = document.getElementById('email_input').value;
     const password = document.getElementById('password_input').value;
+    if(email.trim() === '' || password.trim() === ''){
+        console.log('Please fill all the fields');
+        $('#errorMensaje').text('Por favor, rellene todos los campos');
+        return;
+    }
     fetch('/api/login/', {
         method: 'POST',
         headers: {
@@ -16,6 +21,7 @@ document.getElementById('login_form').addEventListener('submit', function(event)
     .then(data => {
         if (data.error) {
             console.log(data.error);
+            $('#errorMensaje').text('Correo electrónico o contraseña incorrecta');
         } else {
             console.log('User logged in');
         }
